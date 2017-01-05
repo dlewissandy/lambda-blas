0f51e5525d3b63efaae5085393e31a23e6613a4c
Darrell Lewis-Sandy <dlewissandy@gmail.com>
2017-01-05 07:46:57 -0800

    Merged ISSUE-2
    commit ec5c59d467c0d5dc3f962fb3301b0a1fb8952d05
    Author: Darrell Lewis-Sandy <dlewissandy@gmail.com>
    Date:   Thu Jan 5 07:40:50 2017 -0800
    
        Addressed comments from ISSUE-2 pull request
    
    commit 3306071593cc5329ba770d43910045c7b6ad5d11
    Author: Darrell Lewis-Sandy <dlewissandy@gmail.com>
    Date:   Wed Jan 4 14:07:29 2017 -0800
    
        initialized project and created stubs

---
+++ src/Numerical/BLAS.hs
@@ -0,0 +1,11 @@
+module Numerical.BLAS(
+    --module Numerical.BLAS.Complex,
+    --module Numerical.BLAS.Double,
+    --module Numerical.BLAS.DoubleComplex,
+    module Numerical.BLAS.Single,
+    ) where
+
+--import Numerical.BLAS.Complex
+--import Numerical.BLAS.Double
+--import Numerical.BLAS.DoubleComplex
+import Numerical.BLAS.Single
---
+++ test/Main.hs
@@ -0,0 +1,6 @@
+-- | This test suite performs unit tests to evidence the correctness of
+-- the native haskell implementations of BLAS subroutines.
+module Main( main ) where
+
+main :: IO ()
+main = return ()
